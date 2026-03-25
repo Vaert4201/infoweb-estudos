c76c286a528c03cc19bbcbf3edc8ce4d649a07ad
Vaert4201 <vitor.mello1@hotmail.com>
2026-03-24 23:51:45 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,47 @@
+print('Olá Mundo!')
+
+
+
+
+print('Vitor Carvalho Raposo E Mello')
+
+
+
+print('Nome: Vitor')
+print('Idade: 16')
+print('Curso: Infoweb')
+
+
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+
+print('*****')
+print('*   *')
+print('*****')
+
+
+print('Python é incrível')
+
+
+
+print('Programar é divertido!')
+
+
+print(10, 20, sep = ',')
+
+
+print('2')
+print('4')
+print('6')
+print('8')
+print('10')
+
+
+print('Produto  Preço')
+print('Caneta   2.50')
+print('Caderno  12.00')
+print('Borracha  1.75')
---
+++ variaveis.py
@@ -0,0 +1,55 @@
+nome ='Maria'
+print(nome)
+
+
+cidade = 'Natal'
+estado = 'RN'
+print('Eu moro em' , cidade, ',' ,estado, end='.')
+
+      
+
+
+
+ano_nascimento = '2010'
+print(ano_nascimento)
+
+
+
+a = 5
+print('a=', a)
+b = 10
+print('b =', b)
+a, b = b, a
+print('a =', a)
+print('b =', b)
+
+
+
+x = 1
+print('x =', x)
+y = 2
+print('y =', y)
+z = 3
+print('z =', z)
+a = x + y + z
+print('x + y + z =', a)
+
+
+
+preco=49.90 
+print('O preço do produto é', preco)
+
+
+
+
+ativo = True
+print(ativo)
+
+
+mensagem = 'Aluno: João'
+frase= 'Idade: 16' #int
+texto= 'Nota: 8.5' #float
+
+print(mensagem) #imprime Aluno: João
+print(frase)  #imprime Idade: 16
+print(texto) #imprime Nota: 8.5
