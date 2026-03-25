b76047b95c8dc0ea920e9fb691b588b103eaae9e
Vaert4201 <vitor.mello1@hotmail.com>
2026-03-24 23:48:48 -0300

    Add files via upload

---
+++ input.py
@@ -0,0 +1,22 @@
+nome = input('Digite seu nome:')
+print('Olá,', {nome})
+
+
+cidade = input('Digite sua cidade')
+print('Você mora em', {cidade})
+
+
+num1 = input('Digite seu primeiro número:')
+num1 = int(num1)
+num2 = input('Digite seu segundo número:')
+num2 = int(num2)
+print('Seu primeiro número é:', num1)
+print('Seu segundo número é:', num2)
+
+
+nome = input('Digite seu nome:')
+idade = input('Digite sua idade:')
+idade =int(idade)
+print({nome}, 'tem ', {idade},' anos')
+
+data = input()
