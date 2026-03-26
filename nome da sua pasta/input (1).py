b2e8c8fc3658f990894206c640c933992f751144
Vaert4201 <vitor.mello1@hotmail.com>
2026-03-26 02:01:28 -0300

    Add files via upload

---
+++ nome da sua pasta/input (1).py
@@ -0,0 +1,40 @@
+nome = input('Digite seu nome:')
+print('Olá,', {nome})
+
+
+cidade = input('Digite sua cidade')
+print('Você mora em', {cidade})
+
+
+num1 = input('Digite seu primeiro número:')
+num1 = int(num1)
+num2 = input('Digite seu segundo número:')
+num2 = int(num2)
+print('Seu primeiro número é:', num1)
+print('Seu segundo número é:', num2)
+
+
+nome = input('Digite seu nome:')
+idade = input('Digite sua idade:')
+idade =int(idade)
+print({nome}, 'tem ', {idade},' anos')
+
+data = input('Digite seu ano de nascimento:')
+data =int(data)
+print('O ano que você nasceu foi:', {data})
+
+
+palavra=input('Digite qualquer palavra:')
+print('Você digitou:', {palavra})
+
+
+
+
+nome = ('Digite o nome do seu produto:')
+preço = ('Digite o valor do seu produto:')
+preço = float(preço)
+print = ('Produto:', {nome})
+print = ('Preço:', {preço})
+
+
+
