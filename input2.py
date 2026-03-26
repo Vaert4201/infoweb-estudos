1c7bf9aba034a6062b7cfb9e0d41b7f65163cabd
Vaert4201 <vitor.mello1@hotmail.com>
2026-03-26 02:00:38 -0300

    Add files via upload

---
+++ input2.py
@@ -0,0 +1,24 @@
+produto = input("Digite o nome do produto: ")
+preco = float(input("Digite o preço do produto: "))
+
+print(f"Produto:", produto)
+print(f'Preço: R$', preco)
+
+nota1 = float(input('Digite o valor da sua primeira nota:'))
+nota2 = float(input('Digite o valor da sua segunda nota:'))
+nota3 = float(input('Digite o valor da sua terceira nota:'))
+print(f'A sua primeira nota foi:', {nota1})
+print(f'A sua segunda nota foi:', {nota2})
+print(f'A sua terceira nota foi:', {nota3})
+
+
+
+nome1 = input('Digite o seu nome:')
+nome2 = input('Digite o seu nome:')
+print(f'Pessoa 1:', {nome1})
+print(f'Pessoa 2:', {nome2})
+
+
+
+num = float(input('Digite um número inteiro:'))
+print('O seu número é:', {num})
---
+++ operações aritméticas.py
@@ -0,0 +1,65 @@
+num1 = int(input('Digite o seu primeiro número:'))
+num2 = int(input('Digite o seu segundo número:'))
+soma = num1 + num2
+print(f'A soma é:', {soma})
+
+
+num1 = int(input('Digite o seu primeiro número:'))
+num2= int(input('Digite o seu segundo número:'))
+subtração = num1 - num2
+print(f'A subtração é:', {subtração})
+
+
+num1 = float(input('Digite o seu primeiro número:'))
+num2 = float(input('Digite o seu segundo número:'))
+produto = num1 * num2
+print('A multiplicação é:', {produto})
+
+
+num1 = float(input('Digite o seu primeiro número:'))
+num2 = float(input('Digite o seu segundo número:'))
+divisão = num1 / num2
+print('A divisão é:', {divisão})
+
+
+num1 = int(input('Digite o seu primeiro número:'))
+num2 = int(input('Digite o seu segundo número:'))
+divisão = num1 // num2
+print('A divisão inteira é:', {divisão})
+
+
+num1 = int(input('Digite o seu primeiro número:'))
+num2 = int(input('Digite o seu segundo número:'))
+módulo = num1 % num2
+print('O seu resto é:', {módulo})
+
+
+num1 = int(input('Digite a sua base:'))
+num2 = int(input('Digite a sua potência:'))
+potência = num1 ** num2
+print('O quadrado é', {potência})
+
+
+num1 = int(input('Digite a sua base:'))
+num2 = int(input('Digite a sua potência:'))
+potência = num1 ** num2
+print(f'O cubo é:', {potência})
+
+
+nota1 = float(input('Digite a sua primeira nota:'))
+nota2 = float(input('Digite a sua segunda nota:'))
+nota3 = float(input('Digite a sua terceira nota:'))
+media = (nota1 + nota2 + nota3) / 3
+print(f'A sua média é: {media:.2f}')
+
+
+salario = int(input('O valor do salário do funcionário é:'))
+bonus = salario * 1.15
+print('O salário do funcionário com bônus é:', {bonus})
+
+
+
+
+
+
+                   
