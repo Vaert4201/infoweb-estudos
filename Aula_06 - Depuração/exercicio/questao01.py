e8225104b91af0eee1694245d4cbc3b1647a6a8a
Vaert4201 <vitor.mello1@hotmail.com>
2026-05-05 10:25:43 -0300

    Add files via upload

---
+++ Aula_06 - Depuração/exercicio/questao01.py
@@ -0,0 +1,14 @@
+#Erro De Sintaxe-
+#nome = input()
+#if nome == "Paulo":
+   #print("Oi, Paulo")
+#else:
+   #print("Você não está registrado")
+
+nome = input()
+if nome == 'Paulo':
+    print('Oi, Paulo.')
+else:
+    print('Você não está registrado.')
+    
+# o print não estava espaçado devidamente.
---
+++ Aula_06 - Depuração/exercicio/questao03.py
@@ -0,0 +1,13 @@
+# Erro de programação - 
+#numero = int(input('Digite um inteiro: '))
+#if numero < 5:
+   #print('A entrada é maior do que cinco')
+#else:
+   #print('A entrada é menor ou igual a cinco')
+
+numero = int(input('Digite um inteiro: '))
+if numero > 5:
+   print('A entrada é maior do que cinco')
+else:
+   print('A entrada é menor ou igual a cinco')
+# O sinal estava trocado, pois ao invés de mostrar que o número seria maior estava como menor.
