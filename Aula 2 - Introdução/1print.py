2e8b976eaddee32c30e9395371d6804bc0c88b36
Vaert4201 <vitor.mello1@hotmail.com>
2026-05-19 23:11:50 -0300

    Add files via upload

---
+++ Aula 2 - Introdução/1print.py
@@ -0,0 +1,55 @@
+#questao01-
+
+print('Olá Mundo!')
+
+
+#questao02-
+
+print('Vitor Carvalho Raposo E Mello')
+
+#questao03-
+
+print('Nome: Vitor')
+print('Idade: 16')
+print('Curso: Infoweb')
+
+#questao04-
+
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+#questao05-
+
+print('*****')
+print('*   *')
+print('*****')
+
+#questao06-
+
+print('Python é incrível')
+
+#questao07-
+
+print('Programar é divertido!')
+
+#questao08-
+
+print(10, 20, sep = ',')
+
+#questao09-
+
+print('2')
+print('4')
+print('6')
+print('8')
+print('10')
+
+#questao10-
+
+print('Produto  Preço')
+print('Caneta   2.50')
+print('Caderno  12.00')
+print('Borracha  1.75')
---
+++ Aula 2 - Introdução/2variaveis.py
@@ -0,0 +1,61 @@
+#questao11-
+
+nome ='Maria'
+print(nome)
+
+#questao12-
+
+cidade = 'Natal'
+estado = 'RN'
+print('Eu moro em' , cidade, ',' ,estado, end='.')
+
+      
+#questao13-
+
+ano_nascimento = '2010'
+print(ano_nascimento)
+
+#questao14-
+
+a = 5
+print('a=', a)
+b = 10
+print('b =', b)
+a, b = b, a
+print('a =', a)
+print('b =', b)
+
+#questao15-
+
+x = 1
+print('x =', x)
+y = 2
+print('y =', y)
+z = 3
+print('z =', z)
+a = x + y + z
+print('x + y + z =', a)
+
+#questao16-
+
+mensagem = 'Aprendendo Python.'
+print(mensagem)
+
+#questao17-
+
+preco=49.90 
+print('O preço do produto é', preco)
+
+#questao18-
+
+ativo = True
+print(ativo) 
+
+#questao19-
+
+pontos = 100
+pontos = pontos + 50
+print = (pontos)
+
+#questao20-
+
